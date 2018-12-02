6fcdd4ae33609e9421a69fb16222bede4cf098bd
gatonegro95 <ricky395@gmail.com>
2018-12-02 16:31:46 +0100

    Arreglito coger objeto

--- Assets/Code/CharacterGrabObjects.cs
+++ Assets/Code/CharacterGrabObjects.cs
@@ -2,21 +2,79 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterGrabObjects : MonoBehaviour {
+public class CharacterGrabObjects : MonoBehaviour
+{
 
     public Transform finalTransform;
 
     private GameObject pickObject;
 
-  
+    private bool objectPicked;
 
-    private void OnTriggerStay(Collider other)
+    //private void OnTriggerStay(Collider other)
+    //{
+    //   if(other.CompareTag("PickableObject") && Input.GetKeyDown(KeyCode.E))
+    //    {
+    //        pickObject = other.gameObject;
+    //    }
+
+    //}
+
+    private void OnTriggerEnter(Collider other)
     {
-       if(other.CompareTag("PickableObject") && Input.GetKeyDown(KeyCode.E))
+        if(other.CompareTag("PickableObject"))
         {
             pickObject = other.gameObject;
         }
+    }
 
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("PickableObject"))
+        {
+            pickObject = null;
+        }
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            // Suelta el obj
+            if (objectPicked)
+            {
+                objectPicked = false;
+                pickObject.GetComponent<Rigidbody>().isKinematic = false;
+                pickObject.transform.SetParent(null);
+
+            }
+            // Coge el obj
+            else if(pickObject != null)
+            {
+                objectPicked = true;
+                pickObject.GetComponent<Rigidbody>().isKinematic = true;
+                pickObject.transform.position = finalTransform.position;
+                pickObject.transform.SetParent(finalTransform);
+            }
+        }
+       
+        //if( pickObject != null)
+        //{
+
+        //    pickObject.GetComponent<Rigidbody>().isKinematic = true;
+        //    pickObject.transform.position = finalTransform.position;
+
+        //    if (Input.GetKeyDown(KeyCode.E)) {
+        //        // aqui entra cuando lo coges >:D
+        //        print("soltar");
+        //    }
+        //}
+        //else
+        //{
+        //    pickObject.GetComponent<Rigidbody>().isKinematic = false;
+        //    pickObject   = null;
+        //}
     }
 
 
@@ -45,30 +103,6 @@ public class CharacterGrabObjects : MonoBehaviour {
     //　▀█▄　　　　　　█▀　　　▀█　　　　　▄█▀
     //　　　▀█▄▄▄█▀　　　　　　▀█▄▄▄█▀
 
-
-
-
-    private void Update()
-    {
-       
-       
-        if( pickObject != null)
-        {
-
-            pickObject.GetComponent<Rigidbody>().isKinematic = true;
-            pickObject.transform.position = finalTransform.position;
-
-            if (Input.GetKeyDown(KeyCode.E)) {
-                // aqui entra cuando lo coges >:D
-                print("soltar");
-            }
-        }
-        else
-        {
-            pickObject.GetComponent<Rigidbody>().isKinematic = false;
-            pickObject   = null;
-        }
-    }
 
 
 }
