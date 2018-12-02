05c6458c1a416def9387cdb5d8fb779bdb512654
Alejandro <benitezdev@gmail.com>
2018-12-02 13:28:40 +0100

    movimiento del player chachi

--- Assets/Code/CharacterMovement.cs
+++ Assets/Code/CharacterMovement.cs
@@ -7,32 +7,41 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
 
-    public float speed = 6.0f;
-    public float jumpSpeed = 8.0f;
-    public float gravity = 20.0f;
+    [SerializeField] private  float speed     = 6.0f  ;
+    [SerializeField] private  float jumpSpeed = 8.0f  ;
+    [SerializeField] private  float gravity   = 20.0f ;
+    [SerializeField] private  float angularSpeed = 10f;
+
 
     private Vector3 moveDirection = Vector3.zero;
+
     private CharacterController controller;
 
     private bool facingRight;
 
-    private Transform tr;
+    public Transform body;
+    
 
     void Start()
     {
-        tr = transform;
-
         controller = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-        //facingRight = (Input.GetAxis("Horizontal") >= 0) ? true : false;
+        facingRight = (Input.GetAxis("Horizontal") > 0) ? true : false;
 
-        //if (!facingRight)
-        //{
-        //    tr.localScale = new Vector3(-tr.localScale.x, 1, 1);
-        //}
+        if (Input.GetAxis("Horizontal") == 0) { /* NEPE*/ }
+        else if (!facingRight)
+        {
+            body.rotation = Quaternion.Lerp(body.rotation, Quaternion.Euler(0, -180, 0), Time.deltaTime * angularSpeed);
+            // body.localRotation = Quaternion.Euler( 0, 180 , 0);   << Cutre
+        }
+        else
+        {
+            body.rotation = Quaternion.Lerp(body.rotation, Quaternion.Euler(0, 0, 0), Time.deltaTime*angularSpeed);
+            //  body.localRotation = Quaternion.Euler(0, 0, 0);      << Cutre
+        }
 
         if (controller.isGrounded)
         {
