117e12875f61da54c1eaebb6d62e36a066d720c5
Alejandro <benitezdev@gmail.com>
2018-12-02 14:45:57 +0100

    Casi mecanica de coger y rabo for you

---
+++ Assets/Code/CharacterGrabObjects.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterGrabObjects : MonoBehaviour {
+
+    public Transform finalTransform;
+
+    private GameObject pickObject;
+
+  
+
+    private void OnTriggerStay(Collider other)
+    {
+       if(other.CompareTag("PickableObject") && Input.GetKeyDown(KeyCode.E))
+        {
+            pickObject = other.gameObject;
+        }
+
+    }
+
+
+
+
+
+    //    　　　　　　　　▄█▀█▀█▄
+    //　　　　　　　　▄█▀　　█　　▀█▄
+    //　　　　　　　▄█▀　　　　　　　▀█▄
+    //　　　　　　　█　　　　　　　　　█
+    //　　　　　　　█　　　　　　　　　█
+    //　　　　　　　▀█▄▄　　█　　　▄█▀
+    //　　　　　　　　　█　　▄▀▄　　█
+    //　　　　　　　　　█　▀　　　▀　█
+    //　　　　　　　　　█　　　　　　　█
+    //　　　　　　　　　█　　　　　　　█
+    //　　　　　　　　　█　　　　 　　 █
+    //　　　　　　　　　█　　　　　　　█
+    //　　　　　　　　　█　　　　　　　█
+    //　　　▄█▀▀█▄█▀▀▀▀　　　　　　　█▄█▀█▄
+    //　▄█▀▀　　　　      　　　　　　　　　▀▀█
+    //█▀　　　　　　　　　　　　　　　　　　　▀█
+    //█　　　　　　　　　　　　　　　　　　　　　█
+    //█　　　　　　　　　　　▄█▄　　　　　　　　　█
+    //▀█　　　　　　　　　█▀　▀█　　　　　　　　█▀
+    //　▀█▄　　　　　　█▀　　　▀█　　　　　▄█▀
+    //　　　▀█▄▄▄█▀　　　　　　▀█▄▄▄█▀
+
+
+
+
+    private void Update()
+    {
+       
+       
+        if( pickObject != null)
+        {
+
+            pickObject.GetComponent<Rigidbody>().isKinematic = true;
+            pickObject.transform.position = finalTransform.position;
+
+            if (Input.GetKeyDown(KeyCode.E)) {
+                // aqui entra cuando lo coges >:D
+                print("soltar");
+            }
+        }
+        else
+        {
+            pickObject.GetComponent<Rigidbody>().isKinematic = false;
+            pickObject   = null;
+        }
+    }
+
+
+}
